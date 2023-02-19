bcd4a0858f333f979c17b8bf7059e3746d768b21
B20DCPT094 <wuochuy02@gmail.com>
2023-02-19 08:31:58 +0700

    PY01011-LIETKESODEP.py

---
+++ PY01011-LIETKESODEP.py
@@ -0,0 +1,23 @@
+def palind(n):
+    m, tmp = n, 0
+    while m != 0:
+        tmp = tmp * 10 + m % 10
+        m //= 10
+    return tmp == n
+
+def check(n):
+    cnt = 0
+    while n != 0:
+        r = n % 10
+        if r % 2 != 0: return False
+        cnt += 1
+        n //= 10
+    return cnt % 2 == 0
+
+if __name__ == '__main__':
+    TC = int(input())
+    for t in range(TC):
+        n = int(input())
+        for x in range(1, n):
+            if check(x) and palind(x): print(x, end = ' ')
+        print()
