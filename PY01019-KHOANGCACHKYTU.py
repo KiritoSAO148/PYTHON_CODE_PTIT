7271e4de165ff9486fca88d0b572616afcbb6316
B20DCPT094 <wuochuy02@gmail.com>
2023-02-21 08:22:34 +0700

    PY01019-KHOANGCACHKYTU.py

---
+++ PY01019-KHOANGCACHKYTU.py
@@ -0,0 +1,12 @@
+def check(s1, s2):
+    for i in range(1, len(s1)):
+        if abs(ord(s1[i]) - ord(s1[i - 1])) != abs(ord(s2[i]) - ord(s2[i - 1])): return False
+    return True
+
+if __name__ == '__main__':
+    TC = int(input())
+    for t in range(TC):
+        s1 = input()
+        s2 = s1[::-1]
+        if check(s1, s2): print('YES')
+        else: print('NO')
