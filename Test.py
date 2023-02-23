eed0076f1d306e7cb8ffb8334083e6c687fce421
B20DCPT094 <wuochuy02@gmail.com>
2023-02-23 19:31:33 +0700

    Test.py

---
+++ Test.py
@@ -0,0 +1,6 @@
+def make_shirt(msg, size):
+    print(msg, size)
+
+if __name__ == '__main__':
+    make_shirt('The size of shirt that you bought is', 38)
+    make_shirt(size = 40, msg = 'The size of shirt you buy is')
