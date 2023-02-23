6c572e54815ab3816311a0020dab5075b2448988
B20DCPT094 <wuochuy02@gmail.com>
2023-02-23 17:30:13 +0700

    PY01041-SOTANGGIAM.py

---
+++ PY01041-SOTANGGIAM.py
@@ -0,0 +1,14 @@
+def check(n):
+    if len(n) < 3: return False
+    if n[0] >= n[1] or n[-1] >= n[-2]: return False
+    l, r = 0, len(n) - 1
+    while l < r and n[l] < n[l + 1]: l += 1
+    while r > l and n[r] < n[r - 1]: r -= 1
+    return l == r
+
+if __name__ == '__main__':
+    TC = int(input())
+    for t in range(TC):
+        n = input()
+        if check(n): print('YES')
+        else: print('NO')
