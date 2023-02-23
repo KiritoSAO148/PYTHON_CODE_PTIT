9c6d4ef857c9aae1f683f6f36fdc9cf77b0fc8d2
B20DCPT094 <wuochuy02@gmail.com>
2023-02-23 19:30:34 +0700

    PY01010-DAUCUOI.py

--- PY01010-DAUCUOI.py
+++ PY01010-DAUCUOI.py
@@ -2,6 +2,5 @@ if __name__ == '__main__':
     TC = int(input())
     for t in range(TC):
         n = input()
-        x, y = n[:2], n[-2:]
-        if x == y: print('YES')
+        if n[:2] == n[-2:]: print('YES')
         else: print('NO')
