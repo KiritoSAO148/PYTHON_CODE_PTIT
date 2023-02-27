3ea14dfbcbde692ebadd40a07784fc89978fe4ee
B20DCPT094 <wuochuy02@gmail.com>
2023-02-27 15:37:09 +0700

    ICPC0105-TIMSOLONNHAT.py

---
+++ ICPC0105-TIMSOLONNHAT.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    TC = int(input())
+    for t in range(TC):
+        s = input()
+        ans, res = 0, -10 ** 500
+        for i in range(len(s)):
+            if s[i].isdigit(): ans = ans * 10 + (ord(s[i]) - 48)
+            else:
+                if ans > res and ans != 0: res = ans
+                ans = 0
+        if ans > res and ans != 0: res = ans
+        print(res)
