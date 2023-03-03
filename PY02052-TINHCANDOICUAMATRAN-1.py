73cc8673655705218814a73e6be3fd17756cd428
B20DCPT094 <wuochuy02@gmail.com>
2023-03-03 17:16:59 +0700

    PY02052-TINHCANDOICUAMATRAN-1.py

---
+++ PY02052-TINHCANDOICUAMATRAN-1.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n): a[i] = list(map(int, input().split()))
+    k = int(input())
+    s1, s2 = 0, 0
+    for i in range(n):
+        for j in range(n):
+            if j < i: s1 += a[i][j]
+            elif j > i: s2 += a[i][j]
+    if abs(s1 - s2) <= k: print('YES')
+    else: print('NO')
+    print(abs(s1 - s2))
