f2024e56d9f91fc54e6d95743102c09f55fad845
B20DCPT094 <wuochuy02@gmail.com>
2023-03-07 07:42:13 +0700

    PYKT099-HIEUCUAHAITAPTU.py

--- PYKT099-HIEUCUAHAITAPTU.py
+++ PYKT099-HIEUCUAHAITAPTU.py
@@ -5,11 +5,7 @@ import array as arr
 if __name__ == '__main__':
     f1 = open('DATA1.in', 'r')
     f2 = open('DATA2.in', 'r')
-    a, b = f1.read().split(), f2.read().split()
-    for i in range(len(a)): a[i] = a[i].lower()
-    for i in range(len(b)): b[i] = b[i].lower()
-    a = set(a)
-    b = set(b)
+    a, b = set(f1.read().lower().split()), set(f2.read().lower().split())
     for x in sorted(list(a - b)): print(x, end = ' ')
     print()
     for x in sorted(list(b - a)): print(x, end = ' ')
