5456568b5a400c8d986a1e866ba3753cd6bf7615
B20DCPT094 <wuochuy02@gmail.com>
2023-03-10 19:26:54 +0700

    ICPC0105-TIMSOLONNHAT.py

--- ICPC0105-TIMSOLONNHAT.py
+++ ICPC0105-TIMSOLONNHAT.py
@@ -1,12 +1,11 @@
+from sys import stdin
+
 if __name__ == '__main__':
-    TC = int(input())
-    for t in range(TC):
-        s = input()
-        ans, res = 0, -10 ** 500
-        for i in range(len(s)):
-            if s[i].isdigit(): ans = ans * 10 + (ord(s[i]) - 48)
-            else:
-                if ans > res and ans != 0: res = ans
-                ans = 0
-        if ans > res and ans != 0: res = ans
-        print(res)
+    for t in range(int(stdin.readline())):
+        s = stdin.readline()
+        res = ''
+        for x in s:
+            if x.isalpha(): res += ' '
+            else: res += x
+        a = map(int, res.split())
+        print(max(a))
