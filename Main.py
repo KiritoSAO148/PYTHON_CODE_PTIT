da73d75149b8c390b21aa465ee70b559fb05b0f0
B20DCPT094 <wuochuy02@gmail.com>
2023-03-29 22:33:37 +0700

    Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+import Signal_Object
+
+def main():
+    Signal_Object.operation()
+    # Signal_Object.mul_const_operation(5)
+    # Signal_Object.inverse()
+    # Signal_Object.delay(3)
+    # Signal_Object.early(3)
+    # Signal_Object.energy_power()
+    # Signal_Object.convolve()
+    # Signal_Object.dft()
+    # Signal_Object.idft()
+    # Signal_Object.fft()
+    # Signal_Object.ifft()
+    # Signal_Object.dct()
+    # Signal_Object.cross_correlation()
+    # Signal_Object.auto_correlation()
