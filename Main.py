5d003493ef3cbfc9f11148ebd4ccd596fc81d9cf
B20DCPT094 <wuochuy02@gmail.com>
2023-04-02 00:31:54 +0700

    Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import Signal_Object
 
 def main():
-    # Signal_Object.operation()
+    Signal_Object.operation()
     # Signal_Object.mul_const_operation(5)
     # Signal_Object.inverse()
-    # Signal_Object.delay(3)
+    # Signal_Object.delay(4)
     # Signal_Object.early(3)
     # Signal_Object.energy_power()
     # Signal_Object.convolve()
@@ -12,7 +12,7 @@ def main():
     # Signal_Object.idft()
     # Signal_Object.fft()
     # Signal_Object.ifft()
-    Signal_Object.dct()
+    # Signal_Object.dct()
     # Signal_Object.cross_correlation()
     # Signal_Object.auto_correlation()
     # Signal_Object.example()
