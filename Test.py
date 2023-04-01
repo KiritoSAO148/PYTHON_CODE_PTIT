166b70e5417d7bc3f01179c9dd8589eaffb43c19
B20DCPT094 <wuochuy02@gmail.com>
2023-04-02 00:33:09 +0700

    Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,64 @@
-def make_shirt(msg, size):
-    print(msg, size)
+import numpy as np
+# Ví dụ cho phần khởi tạo và getter, setter
+# amplitude = np.random.randint(low = -10, high = 6, size = 10, dtype = int)
+# pos = 0
 
-if __name__ == '__main__':
-    make_shirt('The size of shirt that you bought is', 38)
-    make_shirt(size = 40, msg = 'The size of shirt you buy is')
+# Ví dụ cho phần phép toán với tín hiệu
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# b = np.array([3, -1, 1, 0, 6])
+# pos1 = -3; pos2 = 2
+# const = 5
+
+# Ví dụ cho phần đảo ngược tín hiệu
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
+
+# Ví dụ cho phần làm trễ, sớm tín hiệu
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
+# k = 4
+
+# Ví dụ cho năng lượng và công suất
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
+
+# Ví dụ cho phần tính tích chập
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# b = np.array([3, -1, 1, 0, 6, 5])
+# a = np.random.randint(low = -10000, high = 10000, size = 50001, dtype = int)
+# b = np.random.randint(low=-1000, high=1000, size=1001, dtype=int)
+# pos1 = 3; pos2 = -1
+
+# Ví dụ cho phần biến đổi DFT
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
+
+# Ví dụ cho phần biến đổi IDFT
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# a = np.random.randint(low=-10000, high=10000, size=5001, dtype=int)
+# pos = -2
+
+# Ví dụ cho phần biến đổi FFT
+# a = np.array([-1, 2, -5, 0, -4, 4, -4, 7])
+# a = np.random.randint(low=-10000, high=10000, size=5001, dtype=int)
+# pos = -2
+
+# Ví dụ cho phần biến đổi IFFT
+# a = np.array([-1, 2, -5, 0, -4, 4, -4, 7])
+# a = np.random.randint(low=-10000, high=10000, size=1048576, dtype=int)
+# pos = -2
+
+# Ví dụ cho biến đổi DCT, IDCT
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
+
+# Ví dụ cho phần tương quan chéo
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# b = np.array([3, -1, 1, 0, 6, 5])
+# a = np.random.randint(low = -10000, high = 10000, size = 50001, dtype = int)
+# b = np.random.randint(low=-1000, high=1000, size=1001, dtype=int)
+# pos1 = 3; pos2 = -1
+
+# Ví dụ cho phép tự tương quan
+# a = np.array([-1, 2, -5, 0, -4, 4, -4])
+# pos = -2
