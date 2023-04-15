bc92a12514c088d081e9780c0e9354f59fc11c47
B20DCPT094 <wuochuy02@gmail.com>
2023-04-15 12:25:23 +0700

    Project03.py

---
+++ Project03.py
@@ -0,0 +1,18 @@
+from math import *
+import io, os, sys, time
+import array as arr
+import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import soundfile as sf
+import librosa as lb
+import scipy as sp
+import sympy
+import cmath
+import timeit
+from datetime import datetime
+
+if __name__ == '__main__':
+    n = int(input())
